d692dbb0b511a05abc8123f1c7c2b4dd29757f51
dorydev <95535428+dorydev@users.noreply.github.com>
2023-12-03 11:10:15 +0100

    finish io.c

--- src/io.c
+++ src/io.c
@@ -69,20 +69,34 @@ void gpio_Alt5(unsigned int pin_number)
 
 void uart_init()
 {
-    return;
+    mmio_write(AUX_ENABLES, 1); //enable UART1
+    mmio_write(AUX_MU_IER_REG, 0);
+    mmio_write(AUX_MU_CNTL_REG, 0);
+    mmio_write(AUX_MU_LCR_REG, 3); //8 bits
+    mmio_write(AUX_MU_MCR_REG, 0);
+    mmio_write(AUX_MU_IER_REG, 0);
+    mmio_write(AUX_MU_IIR_REG, 0xC6); //disable interrupts
+    mmio_write(AUX_MU_BAUD_REG, AUX_MU_BAUD(115200));
+    gpio_useAsAlt5(14);
+    gpio_useAsAlt5(15);
+    mmio_write(AUX_MU_CNTL_REG, 3); //enable RX/TX
 }
 
 unsigned int uart_isWriteByteReady()
 {
-    return 0;
+    return mmio_read(AUX_MU_LSR_REG) & 0x20;
 }
 
 unsigned int uart_writeByteBlockingActual(unsigned char ch)
 {
-    return 0;
+    while (!uart_isWriteByteReady()); 
+    mmio_write(AUX_MU_IO_REG, (unsigned int)ch);
 }
 
 void uart_writeText(char *buffer)
 {
-    return;
+    while (*buffer) {
+       if (*buffer == '\n') uart_writeByteBlockingActual('\r');
+       uart_writeByteBlockingActual(*buffer++);
+    }
 }
